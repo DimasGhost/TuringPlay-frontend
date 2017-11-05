ed58b8f33ae222236028123b761ec915199fc056
DimasGhost <strokovghost@gmail.com>
2017-11-05 05:07:21 +0300

    One more step to finish chat

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -1,16 +1,39 @@
 <template>
     <div>
         <modal ref="modal" @modal-out-click="close">
-            <div class="keker"></div>
+            <h1 class="subtitle is-5 chat-header">Chat</h1>
+            <div class="chat-wrapper">
+                <div class="messages-wrapper">
+
+                </div>
+                <input class="chat-input" type="text" placeholder="Type here...">
+            </div>
         </modal>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    .keker {
-        background: black;
-        width: 300px;
-        height: 300px;
+    .chat-header {
+        margin: -5px 0 5px 0;
+    }
+
+    .chat-wrapper {
+        background: #F6F6F6;
+        border: 1px #ededed solid;
+        width: 500px;
+        max-width: 90vw;
+        height: 80vh;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .messages-wrapper {
+        flex-grow: 1;
+    }
+
+    .chat-input {
+        padding: 5px;
+        font-size: 20px;
     }
 </style>
 
